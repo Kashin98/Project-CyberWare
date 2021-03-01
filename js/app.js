30d18c6eb7e16aeb5eefd0aa930df7ca5c711eb3
Karan Kumar S <47289646+Kashin98@users.noreply.github.com>
2021-03-01 11:13:42 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -122,20 +122,21 @@ function addButtonFunctionality(textValue) {
 }
 
 // Btn filter
+const productList = document.querySelector(".productsList");
 const filterBtns = document.querySelectorAll(".Btn");
 filterBtns.forEach((item) => {
   item.addEventListener("click", (e) => {
     const btnValue = e.target.dataset.filter;
     // All Items
-    const implants = document.querySelectorAll(".implants");
+    const implants = productList.querySelectorAll(".implantItem");
     implants.forEach((item) => {
       if (btnValue === "All") {
-        item.style.display = "block";
+        item.parentElement.style.display = "block";
       } else {
         if (item.classList.contains(btnValue)) {
-          item.style.display = "block";
+          item.parentElement.style.display = "block";
         } else {
-          item.style.display = "none";
+          item.parentElement.style.display = "none";
         }
       }
     });
@@ -146,16 +147,16 @@ filterBtns.forEach((item) => {
 const search = document.getElementById("searchBox");
 search.addEventListener("keyup", (e) => {
   const searchValue = e.target.value.trim().toLowerCase();
-  const cardTitle = document.querySelectorAll(".cardHeading");
+  const cardTitle = productList.querySelectorAll(".cardHeading");
   cardTitle.forEach((item) => {
     const titleSlice = item.textContent
       .trim()
       .toLowerCase()
       .slice(0, searchValue.length);
     if (searchValue === titleSlice) {
-      item.parentElement.parentElement.style.display = "block";
+      item.parentElement.parentElement.parentElement.style.display = "block";
     } else {
-      item.parentElement.parentElement.style.display = "none";
+      item.parentElement.parentElement.parentElement.style.display = "none";
     }
   });
 });
@@ -186,3 +187,101 @@ modalClose.addEventListener("click", () => {
 const footerDate = document.getElementById("date");
 const date = new Date();
 footerDate.textContent = date.getFullYear();
+
+// Add new Implants
+class Implant {
+  constructor(type, img, title, description) {
+    this.type = type;
+    this.img = img;
+    this.title = title;
+    this.description = description;
+  }
+  implantDisplay() {
+    const newImplant = document.createElement("div");
+    newImplant.classList.add("implants");
+    newImplant.innerHTML = `
+    <div class="implantItem ${this.type}" data-item="${this.type}">
+          <div class="productImgContainer">
+            <img src="./productImgs/product-${this.img}.png" alt="" />
+            <h1 class="cardHeading">${this.title}</h1>
+            <p class="cardDescription">
+            ${this.description}
+            </p>
+          </div>
+        </div>
+    `;
+    productList.insertAdjacentElement("beforeend", newImplant);
+  }
+}
+
+// Display implants in DOM
+const implant1 = new Implant(
+  "Immune-System",
+  1,
+  "METABOLIC EDITOR",
+  " An implant designed to assist liver and pancreas function against poisons."
+);
+implant1.implantDisplay();
+const implant2 = new Implant(
+  "Nervous-System",
+  2,
+  "Neofiber",
+  " Synthetic muscle fibers."
+);
+implant2.implantDisplay();
+const implant3 = new Implant(
+  "Skeleton-Cyberware",
+  3,
+  "BIONIC JOINTS",
+  "Mechatronic musculoskeletal support."
+);
+implant3.implantDisplay();
+const implant4 = new Implant(
+  "Frontal-Cortex",
+  4,
+  "MEMORY BOOST",
+  " A module that optimizes the sync between brain and cyberware."
+);
+implant4.implantDisplay();
+const implant5 = new Implant(
+  "Immune-System",
+  5,
+  "PAIN EDITOR",
+  " An immune system enhacement valued highly on the streets. Reduce the pain."
+);
+implant5.implantDisplay();
+const implant6 = new Implant(
+  "Immune-System",
+  0,
+  "INDUCTOR",
+  " Invigorates the immune system after receiving an external high-voltage stimulus."
+);
+implant6.implantDisplay();
+const implant7 = new Implant(
+  "Nervous-System",
+  6,
+  "REFLEX TUNER",
+  " A coprocesser that supports decision-making processes in emergency situations."
+);
+implant7.implantDisplay();
+const implant8 = new Implant(
+  "Skeleton-Cyberware",
+  7,
+  "BIONIC LUNGS",
+  "  Efficient lungs engineered with both organic and artificial tissue fibers."
+);
+implant8.implantDisplay();
+const implant9 = new Implant(
+  "Skeleton-Cyberware",
+  8,
+  "DENSE MARROW",
+  "Metal-infused bones capable of withstanding heavy loads."
+);
+implant9.implantDisplay();
+const implant10 = new Implant(
+  "Skeleton-Cyberware",
+  9,
+  "TITANIUM BONES",
+  "Metal-infused bones capable of withstanding heavy loads."
+);
+implant10.implantDisplay();
